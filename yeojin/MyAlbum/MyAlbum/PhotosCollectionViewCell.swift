d283f2aa3464a8bf646f8fca657ca9f09b5bab69
LUCETE012 <LUCETE012+L@gmail.com>
2022-08-09 15:05:19 +0900

    My Album

---
+++ yeojin/MyAlbum/MyAlbum/PhotosCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotosCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by 김여진 on 2022/08/08.
+//
+
+import UIKit
+
+class PhotosCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ yeojin/MyAlbum/MyAlbum/PictureCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PictureCollectionViewCell.swift
+//  MyAlbum
+//
+//  Created by 김여진 on 2022/08/08.
+//
+
+import UIKit
+
+class PictureCollectionViewCell: UICollectionViewCell {
+    
+}
